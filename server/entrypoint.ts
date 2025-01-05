c9dd9eb2b3c6e154f5dba48ef650715b2e803473
yjgaia <yjdev88@gmail.com>
2025-01-05 18:36:19 +0900

    Add Deno server entry points and VSCode settings for project

---
+++ server/entrypoint.ts
@@ -0,0 +1,13 @@
+import { exists } from "https://deno.land/std@0.223.0/fs/mod.ts";
+import { serveFile } from "https://deno.land/std@0.223.0/http/file_server.ts";
+
+Deno.serve(async (req) => {
+  const basePath = Deno.cwd() + "/public";
+  const path = new URL(req.url).pathname;
+  const filePath = basePath + path;
+  if (path !== "/" && await exists(filePath)) {
+    return await serveFile(req, filePath);
+  } else {
+    return await serveFile(req, basePath + "/index.html");
+  }
+});
